0f935f948b66d70fb59b5accf64a20f07b7aebe4
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-09-18 15:02:49 +0300

    Write and style SearchOverlay component to pass Navbar 'search' tests

---
+++ src/components/SearchOverlay/SearchOverlay.jsx
@@ -0,0 +1,47 @@
+import { useNavigate } from 'react-router'
+import logo from '../../assets/logo.png'
+import { Xmark } from 'iconoir-react'
+import styles from './SearchOverlay.module.css'
+import { useState } from 'react'
+import { FocusTrap } from 'focus-trap-react'
+
+function SearchOverlay({ isOpen, onClose }) {
+
+  let navigate = useNavigate()
+
+  const [value, setValue] = useState('')
+
+  return (
+    <FocusTrap active={isOpen}>
+      <div className={`${styles.searchOverlay} ${isOpen ? styles.active : ''}`}>
+        <div className={styles.overlayControls}>
+          <button onClick={() => {
+            navigate('/')
+            onClose();
+            }}
+            className={`${styles.logo}`}>
+            <img src={logo} alt='stolaris'/>
+          </button>
+          <button className={styles.closeButton} onClick={onClose}>
+            <Xmark />
+          </button>
+        </div>
+        <div className={styles.searchContainer}>
+            <label htmlFor="search" className='sr-only'>Search for products</label>
+            <input
+              type="search"
+              id='search'
+              placeholder='Search...'
+              value={value}
+              onChange={(e) => {setValue(e.target.value)}}
+              />
+              <button className={`${styles.clearButton} ${!value ? 'hide' : ''}`} onClick={() => setValue('')}>
+                Erase
+              </button>
+        </div>
+      </div>
+    </FocusTrap>
+  )
+}
+
+export default SearchOverlay
