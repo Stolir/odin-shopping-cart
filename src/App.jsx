174d822e9cc356691dbf2278d14aeda61a989485
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-09-24 16:48:15 +0300

    Adjust App component imports and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import CartProvider from './context/CartContext';
 
 function App() {
   return (
